a10c7a67b7c91a5954f48ee0605405e2af247f17
sivsnkr <ced17i026@iiitdm.ac.in>
2021-11-28 15:47:18 +0530

    initial

---
+++ WebApp/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Homepage from './Components/Homepage';
+
+function App() {
+  return (
+    <div>
+      {/* <canvas id="canvas"></canvas> */}
+      
+      <div>
+        {/* <div class="wave"></div>
+        <div class="wave"></div> */}
+        <Homepage/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
